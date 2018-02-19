19eefb2f2c62edb8cd4f5089ddb516201fb308ce
Ilya Bazhenov <31971067+bazhil@users.noreply.github.com>
2018-02-19 22:35:53 +0300

    Add files via upload

--- vk_api.py
+++ vk_api.py
@@ -5,6 +5,21 @@ AUTH_URL='https://oauth.vk.com/authorize'
 APP_ID=6348055
 
 
+def authorize(self):
+    api_auth_url = AUTH_URL
+    app_id = APP_ID
+    permissions = []
+    # redirect_uri = 'https://oauth.vk.com/blank.html'
+    # display = 'wap'
+    api_version = '5.73'
+
+    auth_url_template = '{0}?client_id={1}&scope={2}&v={5}&response_type=token'
+    auth_url = auth_url_template.format(api_auth_url, app_id, api_version)
+
+    response = requests.get(auth_url)
+    print(response)
+
+
 auth_data={
     'client_id': APP_ID,
     'display': 'page',
